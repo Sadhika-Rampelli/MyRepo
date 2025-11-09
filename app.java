6a9bc90dd9bdbc1e56be210dfcd163cbbfb90d38
root <root@ip-172-31-20-242.ec2.internal>
2025-11-09 09:09:54 +0000

    java code added

---
+++ app.java
@@ -0,0 +1,8 @@
+// This is a simple Java program to print "Hello, World!"
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // System.out.println() is used to print output to the console
+        System.out.println("Hello, World!"); 
+    }
+}
